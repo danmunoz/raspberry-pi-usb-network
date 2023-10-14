265acf4a3a5cf2991d21a753e3c1a8cc29920d9b
RafaelKtistakis <rafaelktistakis@gmail.com>
2023-10-15 00:35:07 +0100

    [WIP] tmux rpi throttled script for dracula status bar

---
+++ include/rpi_throttled.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# setting the locale, some users have issues with different locales, this forces the correct one
+export LC_ALL=en_US.UTF-8
+
+get_throttled() {
+  echo "$(vcgencmd get_throttled | sed 's/throttled=//')"
+}
+
+main() {
+  # storing the refresh rate in the variable RATE, default is 5
+  RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
+  echo "$(get_throttled)"
+  sleep $RATE
+}
+
+# run main driver
+main
