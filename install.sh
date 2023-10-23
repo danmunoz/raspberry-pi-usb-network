17572c983d15ec240b290b2e1bd384d4ad5023cf
RafaelKtistakis <rafaelktistakis@gmail.com>
2023-10-23 20:42:56 +0100

    Added role core under the new collection

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 # Install ansible packages or update them
 
+sudo apt update && sudo apt install -y python3-dev python3-pip libyaml-dev libffi-dev git && sudo pip3 install --no-binary pyyaml ansible
 ansible-galaxy install -r requirements.yml
+ansible-galaxy collection install ./rafaelktistakis
 ansible-playbook main.yml
